1e3a41b9b327b07f9ba0a17f231c5989531c231e
Hamish Sansom <79813180+Hamish01@users.noreply.github.com>
2021-03-30 14:41:04 +1100

    project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace repitition_nested
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name? ");
+            string name = Console.ReadLine();
+            int counter = 0;
+
+            while(counter < 10){
+                counter = counter + 1;
+                int counter2 = 0;
+                while(counter2 < 5){
+                    Console.WriteLine(name);
+                    counter2 = counter2 + 1;
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine("Goodbye " + name);
+        }
+    }
+}
